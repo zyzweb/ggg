d0ff7363176bba7e2627bd4e6e3ffbd4e1eedb83
zyzweb <1609062132@qq.com>
2025-01-23 22:17:36 +0800

    init

---
+++ docs/quickstart.md
@@ -0,0 +1,17 @@
+# 快速上手
+
+### 安装
+
+```bash
+# 通过 npm
+npm i ggg
+
+# 通过 yarn
+yarn add ggg
+
+# 通过 pnpm
+pnpm add ggg
+
+# 通过 bun
+bun add ggg
+```
---
+++ vant.config.mjs
@@ -0,0 +1,39 @@
+export default {
+  name: 'ggg',
+  build: {
+    css: {
+      preprocessor: 'sass',
+    },
+    site: {
+      publicPath: '/ggg/',
+    },
+  },
+  site: {
+    title: 'ggg',
+    logo: 'https://fastly.jsdelivr.net/npm/@vant/assets/logo.png',
+    nav: [
+      {
+        title: '开发指南',
+        items: [
+          {
+            path: 'home',
+            title: '介绍',
+          },
+          {
+            path: 'quickstart',
+            title: '快速上手',
+          },
+        ],
+      },
+      {
+        title: '基础组件',
+        items: [
+          {
+            path: 'demo-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+    ],
+  },
+};
